46aef81551e6d92e8b531f99c0ac59c9411be2a9
Destiny Moore <destinymoore04032001@gmail.com>
2024-08-27 00:06:10 +0100

    Finalised presentation version

--- script.js
+++ script.js
@@ -1,18 +1,16 @@
-console.log("Hello world");
-
 function highlightCurrentNav() {
     const baseUrl = window.location.origin;
     const fullUrl = window.location.href;
 
-    const extention = fullUrl.replace(baseUrl, "");
+    const extension = fullUrl.replace(baseUrl, "");
 
     let nav;
 
-    if (extention === "/") {
+    if (extension === "/") {
         nav = document.getElementById("homeNavLink");
-    } else if (extention === "/articles.html") {
+    } else if (extension === "/articles.html") {
         nav = document.getElementById("articlesNavLink");
-    } else if (extention === "/contact.html") {
+    } else if (extension === "/contact.html") {
         nav = document.getElementById("contactNavLink");
     }    
 
@@ -21,4 +19,26 @@ function highlightCurrentNav() {
     nav.style.fontWeight = "bold";
     nav.style.textShadow = "0 0 5px black";
 }
+
+function contactFormSubmit() {
+    const firstName = document.getElementById("firstNameInput").value;
+    const lastName = document.getElementById("lastNameInput").value;
+    const email = document.getElementById("emailInput").value;
+    const message = document.getElementById("messageInput").value;
+
+    const userSubmission = 
+        `
+        You have submitted the following details: 
+
+        First name: ${firstName}
+        Last name: ${lastName}
+        Email: ${email}
+        Message: ${message}
+        `
+    alert(userSubmission);
+
+    document.getElementById("thankYouMessage").style.display = "block";
+    document.getElementById("sendButton").style.display = "none";
+}
+
 highlightCurrentNav()
